2cd61b08c9a51d99a3be45567e83adce3c38df86
MaryamJalil <maryam7jalil@gmail.com>
2025-10-31 19:04:02 +0100

    completed mutation and queries for lessons

--- src/lessons/lessons.resolver.ts
+++ src/lessons/lessons.resolver.ts
@@ -1,4 +1,29 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query, Int } from '@nestjs/graphql';
+import { LessonsService } from './lessons.service';
+import { LessonModel } from 'src/models/lesson.model';
+import { CreateLessonInput, UpdateLessonInput } from './lesson.input';
 
 @Resolver()
-export class LessonsResolver {}
+export class LessonsResolver {
+  constructor(private readonly lessonService: LessonsService) {}
+
+  @Mutation(() => LessonModel)
+  async createLesson(@Args('input') input: CreateLessonInput) {
+    return this.lessonService.createLesson(input);
+  }
+
+  @Mutation(() => LessonModel)
+  async updateLesson(@Args('input') input: UpdateLessonInput) {
+    return this.lessonService.updateLesson(input);
+  }
+
+  @Query(() => [LessonModel])
+  async lessons() {
+    return this.lessonService.findAlllessons();
+  }
+
+  @Query(() => LessonModel, { nullable: true })
+  async levelById(@Args('id', { type: () => Int }) id: number) {
+    return this.lessonService.findLessonById(id);
+  }
+}
--- src/lessons/lessons.service.ts
+++ src/lessons/lessons.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { CreateLessonInput, UpdateLessonInput } from './lesson.input';
 
 @Injectable()
-export class LessonsService {}
+export class LessonsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+   async createLesson(input: CreateLessonInput) {
+    return this.prisma.lesson.create({
+      data: {
+        title: input.title,
+        content: input.content,
+        order: input.order,
+        course: {
+          connect: { id: input.courseId },
+        },
+      },
+      include: {
+        course: true,
+      },
+    });
+  }
+
+  async updateLesson(input: UpdateLessonInput) {
+    const { id, ...updateData } = input;
+    return this.prisma.lesson.update({
+      where: { id },
+      data: updateData,
+    });
+  }
+
+  async findAlllessons() {
+    return this.prisma.lesson.findMany({
+      include: { level: true },
+    });
+  }
+
+  async findLessonById(id: number) {
+    return this.prisma.lesson.findUnique({ where: { id } });
+  }
+}
